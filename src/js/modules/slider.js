bc357ecc28392cc88c83d5bdfc74f9bd56b9b390
Aleksander Shumeiko <music.dll@mail.ru>
2020-08-04 13:39:21 +0300

    Show small card after 3 seconds

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -10,6 +10,20 @@ export default class Slider {
         if (n > this.slides.length) this.slideIndex = 1;
         if (n < 1) this.slideIndex = this.slides.length;
 
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (e) {}
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
